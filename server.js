9ccccf6946feebdb53c355bb6e5df73db2d951af
Adeel Qureshi <adeelq@pk.ibm.com>
2019-01-27 01:53:46 +0500

    2nd change

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express  = require('express');
 var app = express();
 
 app.get('/', function(req,res){
-    res.json({'msg':'Hello world'});
+    res.json({'msg':'Hello world from docker auto build '});
 });
 
 var port = process.env.port || 3000; 
