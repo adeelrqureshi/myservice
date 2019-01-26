d71c48a0126ec632b3f88870c37f31bd4e608bb8
Adeel Qureshi <adeelq@pk.ibm.com>
2019-01-27 01:31:52 +0500

    first commit app2

---
+++ server.js
@@ -0,0 +1,11 @@
+var express  = require('express');
+var app = express();
+
+app.get('/', function(req,res){
+    res.json({'msg':'Hello world'});
+});
+
+var port = process.env.port || 3000; 
+app.listen(port, function(){
+    console.log('Server started and listening at 3000');
+});
